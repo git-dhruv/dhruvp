004904e067cba4e81cc9aacc1d256b76b75879f9
Dhruv Parikh <36257390+git-dhruv@users.noreply.github.com>
2021-09-21 21:43:52 +0530

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,27 @@
+let modalBtn = document.getElementById("acrylic")
+let modal = document.querySelector(".modal")
+let closeBtn = document.querySelector(".close-btn")
+modalBtn.onclick = function(){
+  modal.style.display = "block"
+}
+closeBtn.onclick = function(){
+  modal.style.display = "none"
+}
+window.onclick = function(e){
+  if(e.target == modal){
+    modal.style.display = "none"
+  }
+}
+
+modalBtn = document.getElementById("uav")
+modalBtn.onclick = function(){
+  modal.style.display = "block"
+}
+closeBtn.onclick = function(){
+  modal.style.display = "none"
+}
+window.onclick = function(e){
+  if(e.target == modal){
+    modal.style.display = "none"
+  }
+}
