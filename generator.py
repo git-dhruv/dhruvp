88c6d531ae2a48dd9c786c25276c1e408f8a3c84
Dhruv Parikh <dhruvparikhrg@gmail.com>
2023-12-24 22:25:55 -0500

    Added generator

--- generator.py
+++ generator.py
@@ -1,11 +1,48 @@
 """
 Project layout generator 
+Its a simple script that gets shit done faster 
 """
 
 
-def main():
-    pass
+def main(name):
+    text = rf'''
+<!-- {name} -->
+<tr onmouseout="{name}_stop()" onmouseover="{name}_start()" class="part">
+    <td style="padding:1px;width:25%;vertical-align:middle">
+    <div class="one">
+        <div class="two" id='{name}_image'>
+        <img src='images/{name}.gif' width="160">
 
+        </div>
+        <img src='images/{name}.gif' width="160">
+    </div>
+    <script type="text/javascript">
+        function {name}_start() {{
+        document.getElementById('{name}_image').style.opacity = "1";
+        }}
+
+        function {name}_stop() {{
+        document.getElementById('{name}_image').style.opacity = "0";
+        }}
+        {name}_stop()
+    </script>
+    </td>
+    <td style="padding:20px;width:75%;vertical-align:middle">
+
+    <papertitle>Pose Graph Optimization</papertitle>
+
+    <br />
+
+    <a href="https://github.com/git-dhruv/Bayesian-informed-6D-Pose-estimation">Code</a>
+
+    <p></p>
+    <p>
+        Stuff 
+    </p>
+    </td>
+</tr>'''
+    with open('out.txt', "w+") as f:
+        f.write(text)
 if __name__ == "__main__":
-    projectName = "bayes6d"
+    projectName = "{name}"
     main(projectName)
